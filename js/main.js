a6f1a59001627efbcf837d1fca505e7d27ab5309
Farkhad012 <pharrellmusicpro@gmail.com>
2023-09-12 19:50:00 +0300

    first task completed

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 "use strict";
+
+let money = 39150;  //  месячный бюджет
+let profit = "фриланс"; //  дополнительный доход
+let expenses = "питание, проезд, квартира";  // дополнительные траты
+let purpose = 200000;    // желаемая сумма накоплений
+let period = 26;    // период накоплений: 24 месяца 
+
+let budgetDay = money/30;
+
+let lenthOfExpenses = expenses.length;
+let lenthOfProfit = profit.length;
+
+console.log(money, profit);
+console.log("Длина строки: " + expenses.length);
+
+console.log("Период равен " + (period) + " месяцев");
+console.log(budgetDay);
