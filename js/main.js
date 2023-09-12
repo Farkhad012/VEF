af2e9324e1e23852ba357dedd6ac1f86cde13188
Farkhad012 <pharrellmusicpro@gmail.com>
2023-09-12 20:38:19 +0300

    the 'expenses' string converted to lower case, the string splitted into an array, result printed to the console

--- js/main.js
+++ js/main.js
@@ -5,15 +5,17 @@ let profit = "фриланс"; //  дополнительный доход
 let expenses = "питание, проезд, квартира";  // дополнительные траты
 let purpose = 200000;    // желаемая сумма накоплений
 let period = 26;    // период накоплений: 24 месяца 
+let budgetDay = money/30;   //  дневной бюджет
 
-let budgetDay = money/30;
+let typeOfExpenses = expenses.length;   //  узнаем длину строки переменной expenses
 
-let typeOfExpenses = expenses.length;
-let lenthOfProfit = profit.length;
+let lowerCaseExpenses = expenses.toLowerCase(); //  приводим к нижнему регистру
+let arrayLowerCaseExpenses = lowerCaseExpenses.split(' '); //   разбиваем строку на массив
 
-console.log(typeof money, typeof profit);
-console.log("Длина строки: " + expenses.length);
+console.log(typeof money, typeof profit);   // тип данных (money, profit)
+console.log("Длина строки: " + expenses.length);    // Длина строки: expenses.length
+console.log("Период равен " + (period) + " месяцев");   // Период равен period месяцев
+console.log("Цель заработать " + (purpose) + " рублей");    //  Цель заработать purpose рублей
+console.log(budgetDay); //  выводим дневной бюджет 
+console.log(arrayLowerCaseExpenses)   // выводим массив
 
-console.log("Период равен " + (period) + " месяцев");
-console.log("Цель заработать " + (purpose) + " рублей");
-console.log(budgetDay);
