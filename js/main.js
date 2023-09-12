29337307813c79a0d709ace259fe2f3c1722cbfa
Farkhad012 <pharrellmusicpro@gmail.com>
2023-09-12 20:09:37 +0300

    printed the data type of variable values 'money, profit' to the console

--- js/main.js
+++ js/main.js
@@ -8,10 +8,10 @@ let period = 26;    // период накоплений: 24 месяца
 
 let budgetDay = money/30;
 
-let lenthOfExpenses = expenses.length;
+let typeOfExpenses = expenses.length;
 let lenthOfProfit = profit.length;
 
-console.log(money, profit);
+console.log(typeof money, typeof profit);
 console.log("Длина строки: " + expenses.length);
 
 console.log("Период равен " + (period) + " месяцев");
